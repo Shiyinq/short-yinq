0a8c5f1142def146ef05c75e5826d14ac54f3a25
Shiyinq <shiyinq.github.io@gmail.com>
2019-12-16 13:24:17 +0700

    add model & migration (users & links)

---
+++ api/database/migrations/2019_12_16_055025_create_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('userId')->nullable();
+            $table->string('idURL',50);
+            $table->string('realURL');
+            $table->bigInteger('countHit');
+            $table->string('status',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
